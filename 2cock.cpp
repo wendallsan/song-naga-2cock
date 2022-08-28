76a3eecaff26be86ba0c75f63a6144913d597586
Dan Steeby <dansteeby@gmail.com>
2022-08-28 15:45:45 -0700

    2cock.cpp: added mux handling, currently set up to handle adsr1 and adsr2 pots. Added adsr2. Added delaySignal ADC Channel for single input (doesn't fit on the mux). Updated DAC to output 2 channels (one for adsr1, on for adsr2). Removed logging.

--- 2cock.cpp
+++ 2cock.cpp
@@ -2,62 +2,122 @@
 #include "daisysp.h"
 using namespace daisy;
 using namespace daisysp;
-DaisySeed hw;
-Adsr adsr1;
-Switch triggerButton1;
-bool buttonIsPressed = false;
 
+#define PIN_ADC_MUX_IN daisy::seed::A0
+#define PIN_ADC_ADSR2_DELAY_IN daisy::seed::A1
+#define MUX_CHANNELS 8
+#define PIN_MUX_SW_1 1
+#define PIN_MUX_SW_2 2
+#define PIN_MUX_SW_3 2
+#define PIN_TRIGGER 16
+
+DaisySeed hw;
+Adsr adsr1, adsr2;
+Switch triggerButton;
+bool buttonIsPressed = false;
 static const size_t kDacBufferSize = 48;
-static uint16_t DMA_BUFFER_MEM_SECTION dac_buffer[kDacBufferSize];
+static uint16_t DMA_BUFFER_MEM_SECTION dac_buffer1[ kDacBufferSize ],
+    dac_buffer2[ kDacBufferSize ];
 
 // SET lastProcessResult TO AN INVALID VALUE SO IT REPORTS THE FIRST TIME IT CHANGES
-float processResult = 0.0, lastProcessResult = 2.0;
+float process1Result = 0.0, 
+    lastProcess1Result = 2.0,
+    process2Result = 0.0,
+    lastProcess2Result = 2.0,
+    adsr1AttackValue,
+    adsr1DecayValue,
+    adsr1SustainValue,
+    adsr1ReleaseValue,
+    adsr2DelayValue,
+    adsr2AttackValue,
+    adsr2DecayValue,
+    adsr2SustainValue,
+    adsr2ReleaseValue;
+
+enum adcChannels {
+	muxSignals,
+    delaySignal,
+	NUM_ADC_CHANNELS
+};
+
+enum muxChannels {
+    ADSR1_ATTACK,
+    ADSR1_DECAY,
+    ADSR1_SUSTAIN,
+    ADSR1_RELEASE,
+    ADSR2_ATTACK,
+    ADSR2_DECAY,
+    ADSR2_SUSTAIN,
+    ADSR2_RELEASE
+};
+
 void handleDac(	uint16_t **out, size_t size ){
     for( size_t i = 0; i < size; i++ ){
         // convert to 12-bit integer (0-4095)
-        processResult = adsr1.Process( triggerButton1.Pressed() );
-        uint16_t value = processResult * 4095.0;
-        out[0][i] = value;
+        process1Result = adsr1.Process( triggerButton.Pressed() );
+        process2Result = adsr2.Process( triggerButton.Pressed() );
+        out[0][i] = process1Result * 4095.0;
+        out[1][i] = process2Result * 4095.0;;
     }
 }
+
+void handleKnobs(){
+    // HANDLE MUXED KNOBS
+    adsr1AttackValue = hw.adc.GetMuxFloat( muxSignals, ADSR1_ATTACK );
+    adsr1DecayValue = hw.adc.GetMuxFloat( muxSignals, ADSR1_DECAY );
+    adsr1SustainValue = hw.adc.GetMuxFloat( muxSignals, ADSR1_SUSTAIN );
+    adsr1ReleaseValue = hw.adc.GetMuxFloat( muxSignals, ADSR1_RELEASE );
+    adsr2AttackValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_ATTACK );
+    adsr2DecayValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_DECAY );
+    adsr2SustainValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_SUSTAIN );
+    adsr2ReleaseValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_RELEASE );
+
+    // HANDLE THE NON-MUXED KNOBS
+    adsr2DelayValue = hw.adc.GetFloat( delaySignal );
+
+    // MAP TIME-RELATED VALUES TO 0 - 4 SECONDS
+    adsr1.SetAttackTime( fmap( adsr1AttackValue, 0.0, 4.0 ) );
+    adsr1.SetDecayTime( fmap( adsr1DecayValue, 0.0, 4.0 ) );
+    adsr1.SetSustainLevel( adsr1SustainValue );
+    adsr1.SetReleaseTime( fmap( adsr1ReleaseValue, 0.0, 4.0 ) );
+
+    adsr2.SetAttackTime( fmap( adsr2AttackValue, 0.0, 4.0 ) );
+    adsr2.SetDecayTime( fmap( adsr2DecayValue, 0.0, 4.0 ) );
+    adsr2.SetSustainLevel( adsr2SustainValue );
+    adsr2.SetReleaseTime( fmap( adsr2ReleaseValue, 0.0, 4.0 ) );
+
+    // TODO: HANDLE DELAY CONTROL
+}
+
 int main( void ){
-    // hw.Configure();
     hw.Init();
     hw.StartLog( true );
-    hw.PrintLine( "logger started." );
+    AdcChannelConfig adcConfig[ NUM_ADC_CHANNELS ];
+    adcConfig[ muxSignals ].InitMux( 
+        PIN_ADC_MUX_IN, 
+        MUX_CHANNELS, 
+        hw.GetPin( PIN_MUX_SW_1 ), 
+        hw.GetPin( PIN_MUX_SW_2 ), 
+        hw.GetPin( PIN_MUX_SW_3 )
+    );
+    adcConfig[ delaySignal ].InitSingle( PIN_ADC_ADSR2_DELAY_IN );
+    hw.adc.Init( adcConfig, NUM_ADC_CHANNELS );
+    hw.adc.Start();
     DacHandle::Config dacConfig;
     dacConfig.target_samplerate = 48000;
     dacConfig.bitdepth = DacHandle::BitDepth::BITS_12;
     dacConfig.buff_state = DacHandle::BufferState::ENABLED;
     dacConfig.mode = DacHandle::Mode::DMA;
-    dacConfig.chn = DacHandle::Channel::ONE;
-    DacHandle::Result dacInitResult = hw.dac.Init( dacConfig );
-    DacHandle::Result dacStartResult = hw.dac.Start( dac_buffer, kDacBufferSize, handleDac );
-    hw.Print( "dac init result: " );
-    hw.PrintLine( dacInitResult == DacHandle::Result::OK? "ok" : "err" );
-    hw.Print( "dac start result: " );
-    hw.PrintLine( dacStartResult == DacHandle::Result::OK? "ok" : "err" );
-    // PIN 15 (AKA D15) IS ACTUALLY PIN 22 ON THE DAISY SEED, RIGHT?
-	triggerButton1.Init( hw.GetPin( 15 ), 100 ); 
-    // adsr1.Init( dacConfig.target_samplerate );
+    dacConfig.chn = DacHandle::Channel::BOTH;
+    hw.dac.Init( dacConfig );
+    hw.dac.Start( dac_buffer1, dac_buffer2, kDacBufferSize, handleDac );
+	triggerButton.Init( hw.GetPin( PIN_TRIGGER ), 10 );
     adsr1.Init( dacConfig.target_samplerate );
-    // STATICALLY SET ADSR VALUES FOR NOW
-    adsr1.SetAttackTime( 0.5 );
-    adsr1.SetDecayTime( 1.0 );
-    adsr1.SetSustainLevel( 0.5 );
-    adsr1.SetReleaseTime( 1.0 );
+    adsr2.Init( dacConfig.target_samplerate );
     while( true ){        
-        triggerButton1.Debounce();
-        if( triggerButton1.Pressed() ){
-            if( buttonIsPressed != true ){
-                buttonIsPressed = true;
-                hw.PrintLine( "pressed!" );
-            }
-        } else buttonIsPressed = false;
-        if( processResult != lastProcessResult ){
-            lastProcessResult = processResult;
-            hw.PrintLine( "process: " FLT_FMT3, FLT_VAR3( processResult ) );
-        }
+        triggerButton.Debounce();
+        buttonIsPressed = ( triggerButton.Pressed() && buttonIsPressed != true )? true : false;
+        handleKnobs();
         System::Delay( 1 );
     }
 }
