27e761451e458243a06ec5900c03c0c78ef66953
Dan Steeby <dansteeby@gmail.com>
2022-09-02 10:10:46 -0700

    delayline is working! Added check for button release. Set delayTime as a class property so it can be set anywhere. Cleaned up setting delayTime from knob code. General code cleanup.

--- 2cock.cpp
+++ 2cock.cpp
@@ -17,12 +17,10 @@ DaisySeed hw;
 Adsr adsr1, adsr2;
 Switch triggerButton;
 static DelayLine<bool, MAX_DELAY> DSY_SDRAM_BSS delayLine;
-
-bool buttonIsPressed = false;
+bool buttonIsPressed = false, lastButtonIsPressed = false;
 static const size_t kDacBufferSize = 48;
 static uint16_t DMA_BUFFER_MEM_SECTION dac_buffer1[ kDacBufferSize ],
     dac_buffer2[ kDacBufferSize ];
-
 // SET lastProcessResult TO AN INVALID VALUE SO IT REPORTS THE FIRST TIME IT CHANGES
 float process1Result = 0.0, 
     lastProcess1Result = 2.0,
@@ -37,14 +35,13 @@ float process1Result = 0.0,
     adsr2DecayValue,
     adsr2SustainValue,
     adsr2ReleaseValue, 
-    lastAdsr2DelayValue = 2.0;
-
+    lastAdsr2DelayValue = 2.0,
+    delayTime = 1.0;
 enum adcChannels {
 	muxSignals,
     delaySignal,
 	NUM_ADC_CHANNELS
 };
-
 enum muxChannels {
     ADSR1_ATTACK,
     ADSR1_DECAY,
@@ -63,9 +60,7 @@ void handleDac( uint16_t **out, size_t size ){
         // CONVERT TO A 12 BIT INTEGER RANGE (0 - 4095) FOR THE DAC
         out[0][i] = adsr1.Process( buttonIsPressed ) * 4095.0;
         // HANDLE OUT 2: READ FROM THE DELAYLINE
-        out[1][i] = adsr2.Process( delayLine.Read() && buttonIsPressed ) * 4095.0;
-        // out[1][i] = adsr2.Process( triggerButton.Pressed() ) * 4095.0;
-        // out[1][i] = out[0][i];
+        out[1][i] = adsr2.Process( delayLine.Read() ) * 4095.0;
     }
 }
 
@@ -79,29 +74,27 @@ void handleKnobs(){
     adsr2DecayValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_DECAY );
     adsr2SustainValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_SUSTAIN );
     adsr2ReleaseValue = hw.adc.GetMuxFloat( muxSignals, ADSR2_RELEASE );
-
-    // HANDLE THE NON-MUXED KNOBS
+    // HANDLE THE NON-MUXED KNOB
     adsr2DelayValue = hw.adc.GetFloat( delaySignal );
-
     // MAP TIME-RELATED VALUES TO 0 - 4 SECONDS
     adsr1.SetAttackTime( fmap( adsr1AttackValue, 0.0, 4.0 ) );
     adsr1.SetDecayTime( fmap( adsr1DecayValue, 0.0, 4.0 ) );
     adsr1.SetSustainLevel( adsr1SustainValue );
     adsr1.SetReleaseTime( fmap( adsr1ReleaseValue, 0.0, 4.0 ) );
-
     adsr2.SetAttackTime( fmap( adsr2AttackValue, 0.0, 4.0 ) );
     adsr2.SetDecayTime( fmap( adsr2DecayValue, 0.0, 4.0 ) );
     adsr2.SetSustainLevel( adsr2SustainValue );
     adsr2.SetReleaseTime( fmap( adsr2ReleaseValue, 0.0, 4.0 ) );
-
     if( adsr2DelayValue != lastAdsr2DelayValue ){ // HANDLE DELAY CONTROL
-        lastAdsr2DelayValue = adsr2DelayValue;
-        float delayTime = fmap( adsr2DelayValue, 0.0, 4.0 ) * SAMPLE_RATE;
-        if( delayTime < 1.0 ) delayTime = 1.0;
+        delayTime = fclamp(
+            fmap( adsr2DelayValue, 0.0, 4.0 ) * SAMPLE_RATE,
+            1.0, 
+            MAX_DELAY
+        );
         delayLine.SetDelay( delayTime );
+        lastAdsr2DelayValue = adsr2DelayValue;
     }
 }
-
 void initADC(){
     AdcChannelConfig adcConfig[ NUM_ADC_CHANNELS ];
     adcConfig[ muxSignals ].InitMux( 
@@ -115,7 +108,6 @@ void initADC(){
     hw.adc.Init( adcConfig, NUM_ADC_CHANNELS );
     hw.adc.Start();
 }
-
 void initDAC(){
     DacHandle::Config dacConfig;
     dacConfig.target_samplerate = SAMPLE_RATE;
@@ -126,26 +118,23 @@ void initDAC(){
     hw.dac.Init( dacConfig );
     hw.dac.Start( dac_buffer1, dac_buffer2, kDacBufferSize, handleDac );
 }
-
 int main( void ){
     hw.Init();
-    hw.StartLog();
     initADC();
     initDAC();
     triggerButton.Init( hw.GetPin( PIN_TRIGGER ), 10 );
     adsr1.Init( SAMPLE_RATE );
     adsr2.Init( SAMPLE_RATE );
     delayLine.Init();
-    int i = 0;
     while( true ){
         triggerButton.Debounce();
         buttonIsPressed = triggerButton.Pressed();
-        handleKnobs();
-        i++;
-        if( i >= 500 ){
-            hw.PrintLine( FLT_FMT3, FLT_VAR3( adsr2DelayValue ) );
-            i = 0;
+        if( !buttonIsPressed && lastButtonIsPressed ){
+            delayLine.Reset();
+            delayLine.SetDelay( delayTime ); // SET THE DELAY TIME AGAIN, SINCE RESET SETS THIS
         }
+        lastButtonIsPressed = buttonIsPressed;
+        handleKnobs();
         System::Delay( 1 );
     }
 }
